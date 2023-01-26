11724ebb14c0c3a61facd2853d24de232241751c
Nooa Jekunen <nopelius.jekunen@gmail.com>
2023-01-26 22:19:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/customalertdialog/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.customalertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] OWNERS = new String[] {
+      "Simo Siivola", "Pasi Poromies", "Lassi Lapinmies", "Laila Lakkasuo", "Repa Revontuli",
+      "Timo Tunturi", "Sakke Suoperä", "Heikki Heinähattu", "Kullervo Kullanhuuhtoja"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button openDialogButton = findViewById(R.id.mark);
+        openDialogButton.setOnClickListener(v -> showDialog());
+    }
+
+    protected void showDialog() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_dropdown_item_1line, OWNERS
+        );
+        AutoCompleteTextView textView = mView.findViewById(R.id.autoCompleteTextView);
+        textView.setAdapter(adapter);
+        Button suggestion1 = mView.findViewById(R.id.suggestion1);
+        suggestion1.setOnClickListener(v -> {
+            textView.setText(suggestion1.getText());
+        });
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+    }
+}
